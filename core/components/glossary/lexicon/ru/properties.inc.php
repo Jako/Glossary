28fa9b08163ee0df346fae1fa7c4e9e270c3fe4e
Thomas Jakobi <thomas.jakobi@partout.info>
2018-12-17 12:33:42 +0100

    New translations properties.inc.php (Russian)

--- core/components/glossary/lexicon/ru/properties.inc.php
+++ core/components/glossary/lexicon/ru/properties.inc.php
@@ -1,8 +1,8 @@
 <?php
-$_lang['glossary.glossary.groupTpl'] = 'Template chunk for glossary item groups';
-$_lang['glossary.glossary.navItemTpl'] = 'Template chunk for each item in the nav-bar';
-$_lang['glossary.glossary.navOuterTpl'] = 'Template chunk for outer nav-bar wrapper';
-$_lang['glossary.glossary.outerTpl'] = 'Template chunk for glossary outer wrapper';
-$_lang['glossary.glossary.showNav'] = 'Shows a quick-nav bar at the top of the glossary';
-$_lang['glossary.glossary.termTpl'] = 'Template chunk for glossary term items';
+$_lang['glossary.glossary.groupTpl'] = 'Чанк для групп терминов';
+$_lang['glossary.glossary.navItemTpl'] = 'Чанк для букв в алфавитном указателе';
+$_lang['glossary.glossary.navOuterTpl'] = 'Чанк для алфавитного указателя';
+$_lang['glossary.glossary.outerTpl'] = 'Чанк для списка терминов';
+$_lang['glossary.glossary.showNav'] = 'Показать алфавитный указатель';
+$_lang['glossary.glossary.termTpl'] = 'Чанк для терминов';
 $_lang['glossary.glossary.toPlaceholder'] = 'If set, the snippet result will be assigned to this placeholder instead of outputting it directly.';
