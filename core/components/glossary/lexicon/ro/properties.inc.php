9d67550cddb47c217f63cabb3cb288d6bcc69441
Thomas Jakobi <thomas.jakobi@partout.info>
2018-12-17 12:33:41 +0100

    New translations properties.inc.php (Romanian)

--- core/components/glossary/lexicon/ro/properties.inc.php
+++ core/components/glossary/lexicon/ro/properties.inc.php
@@ -1,8 +1,8 @@
 <?php
-$_lang['glossary.glossary.groupTpl'] = 'Template chunk for glossary item groups';
-$_lang['glossary.glossary.navItemTpl'] = 'Template chunk for each item in the nav-bar';
-$_lang['glossary.glossary.navOuterTpl'] = 'Template chunk for outer nav-bar wrapper';
-$_lang['glossary.glossary.outerTpl'] = 'Template chunk for glossary outer wrapper';
-$_lang['glossary.glossary.showNav'] = 'Shows a quick-nav bar at the top of the glossary';
-$_lang['glossary.glossary.termTpl'] = 'Template chunk for glossary term items';
+$_lang['glossary.glossary.groupTpl'] = 'Chunk-ul pentru grupurile de termeni';
+$_lang['glossary.glossary.navItemTpl'] = 'Chunk-ul pentru literele din index';
+$_lang['glossary.glossary.navOuterTpl'] = 'Chunk-ul pentru indexul alfabetic';
+$_lang['glossary.glossary.outerTpl'] = 'Chunk-ul pentru lista de termeni';
+$_lang['glossary.glossary.showNav'] = 'Afișează indexul alfabetic';
+$_lang['glossary.glossary.termTpl'] = 'Chunk-ul pentru termeni';
 $_lang['glossary.glossary.toPlaceholder'] = 'If set, the snippet result will be assigned to this placeholder instead of outputting it directly.';
