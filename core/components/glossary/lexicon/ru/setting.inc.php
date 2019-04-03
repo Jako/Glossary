6e0871eef37cd1812653dec090012e10028ffc51
Thomas Jakobi <thomas.jakobi@partout.info>
2019-04-03 07:43:51 +0200

    New translations setting.inc.php (Russian)

--- core/components/glossary/lexicon/ru/setting.inc.php
+++ core/components/glossary/lexicon/ru/setting.inc.php
@@ -7,19 +7,19 @@
  */
 $_lang['setting_glossary.debug'] = 'Включить отладку';
 $_lang['setting_glossary.debug_desc'] = 'Добавить информацию отладки в журнал ошибок MODX.';
-$_lang['setting_glossary.disabledAttributes'] = 'Disabled Attributes';
-$_lang['setting_glossary.disabledAttributes_desc'] = '(Comma separated list) Glossary does not replace text inside of this HTML tag attributes.';
+$_lang['setting_glossary.disabledAttributes'] = 'Отключенные атрибуты';
+$_lang['setting_glossary.disabledAttributes_desc'] = '(Список разделенных запятыми) Глоссарий не заменяет текст внутри этого атрибута HTML тегов.';
 $_lang['setting_glossary.fullwords'] = 'Поиск по всему Слова';
 $_lang['setting_glossary.fullwords_desc'] = 'Заменить только полные слова глоссарий термина в содержании ресурса.';
-$_lang['setting_glossary.html'] = 'Allow HTML';
-$_lang['setting_glossary.html_desc'] = 'Allow HTML in the explanation (enables a rich-text editor in the term form).';
+$_lang['setting_glossary.html'] = 'Разрешить HTML';
+$_lang['setting_glossary.html_desc'] = 'Разрешить HTML в объяснение (позволяет редактор форматированного текста в виде термин).';
 $_lang['setting_glossary.resid'] = 'Словарь ресурсов ID';
 $_lang['setting_glossary.resid_desc'] = 'ID страницы на которой находится сниппет Glossary.';
 $_lang['setting_glossary.sections'] = 'Ограничить разделам';
 $_lang['setting_glossary.sections_desc'] = 'Заменить глоссарий терминов только в разделах, помеченных: &lt;!— GlossaryStart --&gt;&lt;!— GlossaryEnd --&gt;.';
-$_lang['setting_glossary.sectionsEnd'] = 'Section End Marker';
-$_lang['setting_glossary.sectionsEnd_desc'] = 'Marker at the end of a section processed by Glossary. The restriction to marked sections can be activated in the setting \'glossary.sections\'.';
-$_lang['setting_glossary.sectionsStart'] = 'Section Start Marker';
-$_lang['setting_glossary.sectionsStart_desc'] = 'Marker at the start of a section processed by Glossary. The restriction to marked sections can be activated in the setting \'glossary.sections\'.';
+$_lang['setting_glossary.sectionsEnd'] = 'Маркер конца раздела';
+$_lang['setting_glossary.sectionsEnd_desc'] = 'Маркер в конце раздела, обработанного глоссарием. Ограничение на отмеченные секции может быть активировано в настройках \'glossary.sections\'.';
+$_lang['setting_glossary.sectionsStart'] = 'Маркер начала раздела';
+$_lang['setting_glossary.sectionsStart_desc'] = 'Маркер в начале раздела, обработанного глоссарием. Ограничение на отмеченные секции может быть активировано в настройках \'glossary.sections\'.';
 $_lang['setting_glossary.tpl'] = 'Выделите шаблон';
 $_lang['setting_glossary.tpl_desc'] = 'Чанк для выделенных терминов.';
