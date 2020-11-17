aa9cfbd3830d38d972a16e13a10240e3295bfbcf
Thomas Jakobi <thomas.jakobi@partout.info>
2020-11-17 21:15:45 +0100

    Update lexicon

--- core/components/glossary/lexicon/en/properties.inc.php
+++ core/components/glossary/lexicon/en/properties.inc.php
@@ -10,5 +10,6 @@ $_lang['glossary.glossary.navItemTpl'] = 'Template chunk for each item in the na
 $_lang['glossary.glossary.navOuterTpl'] = 'Template chunk for outer nav-bar wrapper';
 $_lang['glossary.glossary.outerTpl'] = 'Template chunk for glossary outer wrapper';
 $_lang['glossary.glossary.showNav'] = 'Shows a quick-nav bar at the top of the glossary';
+$_lang['glossary.glossary.showEmptySections'] = 'Show empty sections in the quick-nav bar at the top of the glossary';
 $_lang['glossary.glossary.termTpl'] = 'Template chunk for glossary term items';
 $_lang['glossary.glossary.toPlaceholder'] = 'If set, the snippet result will be assigned to this placeholder instead of outputting it directly.';
