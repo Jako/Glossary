4a5f4debe0e4b815f44d5bf61bafdb405e4f601a
Thomas Jakobi <buero@treehillstudio.de>
2021-05-07 09:58:01 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (8 of 8 strings)
    
    Translation: MODX Extras/MODX Glossary/Properties
    Translate-URL: https://hosted.weblate.org/projects/modx-extras/modx-glossary-properties/ru/

--- core/components/glossary/lexicon/ru/properties.inc.php
+++ core/components/glossary/lexicon/ru/properties.inc.php
@@ -10,6 +10,6 @@ $_lang['glossary.glossary.navItemTpl'] = 'Чанк для букв в алфав
 $_lang['glossary.glossary.navOuterTpl'] = 'Чанк для алфавитного указателя';
 $_lang['glossary.glossary.outerTpl'] = 'Чанк для списка терминов';
 $_lang['glossary.glossary.showNav'] = 'Показать алфавитный указатель';
-$_lang['glossary.glossary.showEmptySections'] = 'Show empty sections in the quick-nav bar at the top of the glossary';
+$_lang['glossary.glossary.showEmptySections'] = 'Показать пустые разделы в панели быстрого доступа в верхней части глоссария';
 $_lang['glossary.glossary.termTpl'] = 'Чанк для терминов';
 $_lang['glossary.glossary.toPlaceholder'] = 'Если установлено, результат сниппета будет назначен этому плейсхолду вместо его прямого вывода.';
