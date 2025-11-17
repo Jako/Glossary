6512b773ad37abfda9728ee246a377173802e957
Thomas Jakobi <buero@treehillstudio.de>
2025-11-17 12:43:17 +0100

    Added translation using Weblate (Spanish)

---
+++ core/components/glossary/lexicon/es/setting.inc.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Setting Lexicon Entries for Glossary
+ *
+ * @package glossary
+ * @subpackage lexicon
+ */
+$_lang['setting_glossary.debug'] = '';
+$_lang['setting_glossary.debug_desc'] = '';
+$_lang['setting_glossary.disabledTags'] = '';
+$_lang['setting_glossary.disabledTags_desc'] = '';
+$_lang['setting_glossary.fullwords'] = '';
+$_lang['setting_glossary.fullwords_desc'] = '';
+$_lang['setting_glossary.html'] = '';
+$_lang['setting_glossary.html_desc'] = '';
+$_lang['setting_glossary.resid'] = '';
+$_lang['setting_glossary.resid_desc'] = '';
+$_lang['setting_glossary.sections'] = '';
+$_lang['setting_glossary.sections_desc'] = '';
+$_lang['setting_glossary.sectionsEnd'] = '';
+$_lang['setting_glossary.sectionsEnd_desc'] = '';
+$_lang['setting_glossary.sectionsStart'] = '';
+$_lang['setting_glossary.sectionsStart_desc'] = '';
+$_lang['setting_glossary.tpl'] = '';
+$_lang['setting_glossary.tpl_desc'] = '';
