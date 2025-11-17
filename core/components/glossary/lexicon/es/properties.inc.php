e32fcdb7994aeb222d36307d6227994dc5f67f64
Thomas Jakobi <buero@treehillstudio.de>
2025-11-17 12:42:56 +0100

    Added translation using Weblate (Spanish)

---
+++ core/components/glossary/lexicon/es/properties.inc.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Properties Lexicon Entries for Glossary
+ *
+ * @package glossary
+ * @subpackage lexicon
+ */
+$_lang['glossary.glossary.groupTpl'] = '';
+$_lang['glossary.glossary.navItemTpl'] = '';
+$_lang['glossary.glossary.navOuterTpl'] = '';
+$_lang['glossary.glossary.outerTpl'] = '';
+$_lang['glossary.glossary.showEmptySections'] = '';
+$_lang['glossary.glossary.showNav'] = '';
+$_lang['glossary.glossary.termTpl'] = '';
+$_lang['glossary.glossary.toPlaceholder'] = '';
